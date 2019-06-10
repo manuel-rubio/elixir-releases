44b742c97def06423bdf2c94164f531dcb22df0b
Manuel Rubio <manuel@altenwald.com>
2019-06-11 01:01:15 +0200

    bugfix upgrade from 2 to 3

--- lib/cuatro/application.ex
+++ lib/cuatro/application.ex
@@ -26,13 +26,21 @@ defmodule Cuatro.Application do
   end
 
   def upgrade do
+    IO.puts "==> Updating supervisor (add Cuatro.Repo)"
     Supervisor.start_child Cuatro.Supervisor,
                            Supervisor.Spec.supervisor(Cuatro.Repo, [])
 
+    # Config environment
+    System.put_env "MNESIA_HOST", to_string(node())
+    File.mkdir_p! Application.get_env(:mnesia, :dir)
+    
     # Create database
+    IO.puts "==> Updating database (create database) #{node()}"
+    Cuatro.Repo.__adapter__.storage_down(Cuatro.Repo.config())
     Cuatro.Repo.__adapter__.storage_up(Cuatro.Repo.config())
 
     # Run migrations
+    IO.puts "==> Running Migrations"
     {:ok, _} = EctoBootMigration.migrate(:cuatro)
   end
 end
